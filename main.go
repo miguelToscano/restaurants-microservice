4c5e6f02382314f1bc74e54889eda90c7b9892b6
miguelToscano <miguetoscano288@gmail.com>
2023-01-31 23:25:26 -0300

    Added tui mode

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/miguelToscano/restaurants-microservice/adapters/inbound/rest"
+	"github.com/miguelToscano/restaurants-microservice/adapters/inbound/tui"
+)
+
+const (
+	TUI_MODE  = "TUI"
+	REST_MODE = "REST"
 )
 
 func main() {
-	rest.Start()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	runMode := os.Getenv("RUN_MODE")
+
+	switch runMode {
+	case TUI_MODE:
+		tui.Start()
+	case REST_MODE:
+		rest.Start()
+	default:
+		fmt.Println("Invalid RUN_MODE env variable")
+	}
+
+	// rest.Start()
+	tui.Start()
 }
